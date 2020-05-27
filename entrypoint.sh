69494938c0024312b5069ebe725d4bd4b2b8bb4a
David Wagner <dwagner86@gmail.com>
2020-05-26 23:07:54 -0400

    moves python libs to system path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ rm -rf /build/*
 # install qtile
 pip3 install --prefix /build/usr qtile
 
+# move python libs to correct path
+mv /build/usr/lib/python3.8 /build/usr/lib/python3
+mv /build/usr/lib/python3/site-packages /build/usr/lib/python3/dist-packages
+
 # add xsession
 mkdir -p /build/usr/share/xsessions
 cp /qtile.desktop /build/usr/share/xsessions/qtile.desktop
@@ -19,7 +23,7 @@ fpm --input-type dir \
   --category x11 \
   --description "A full-featured, hackable tiling window manager written and configured in Python" \
   --url http://www.qtile.org \
-  --version "$(PYTHONPATH=/build/usr/lib/python3.8/site-packages/ /build/usr/bin/qtile --version)" \
+  --version "$(PYTHONPATH=/build/usr/lib/python3/dist-packages/ /build/usr/bin/qtile --version)" \
   --depends python3 \
   --depends python3-xcffib \
   --depends python3-cairocffi \
